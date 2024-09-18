c79354da3a3ea8889a1456a430b67d0d04056d0a
farhan <farhan.gh555@gmail.com>
2024-09-18 22:48:22 +0300

    hello my first commit

---
+++ src/main/java/com/ex/proto/ProtoApplication.java
@@ -0,0 +1,27 @@
+package com.ex.proto;
+
+import org.apache.catalina.webresources.FileResource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.StandardOpenOption;
+
+@SpringBootApplication
+public class ProtoApplication {
+
+	public ProtoApplication() throws FileNotFoundException {
+	}
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(ProtoApplication.class, args);
+		File file = new File("xx.txt") ;
+
+
+		FileSystemResource x = new FileSystemResource(file) ;
+		PrintWriter asd = new PrintWriter(x.getOutputStream(),true) ;
+
+	}}
